e46108212fad686c8653b7124b457cd45d88fc40
kambostrong <71000311+kambostrong@users.noreply.github.com>
2022-10-13 15:29:30 +1300

    finish test compeltely

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
-
+//Write a function that simply repeats the string a given number of times:
+//repeatString('hey', 3) // returns 'heyheyhey'
+//
+let result;
+let resultToAdd;
+const repeatString = function(string, num) {
+    result = string;
+    for (; num > 1; num--){
+        result = result + string;
+    } 
+    if (string == undefined){
+        return result;
+    }
+    if (num === 0){
+        return "";
+    } else if (num < 0){
+        return "ERROR";
+    } else {return result;
+    }
+    
 };
 
 // Do not edit below this line
