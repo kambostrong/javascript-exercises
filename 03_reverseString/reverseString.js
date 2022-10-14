a64d2b5823103602ec5953c2bbb5ce4c347dea2d
kambostrong <71000311+kambostrong@users.noreply.github.com>
2022-10-14 15:47:44 +1300

    passes but without function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,14 +1,34 @@
 let array;
 let result;
+let arrayLength;
+let combinedResult;
+//STRING STARTS AS NORMAL WORD, FEED BACK THE REVERSE
+//EG, it's fed 'hello' and you need to give back 'olleh'
+//As an array, hello has 5 characters which means an array length of 5
+//----- H 0    E 1    L 2    L 3    O 4
+//the reverse would be
+//----- 43210 OLLEH
+
 
 const reverseString = function(string) {
     array = string.split('');
+    //for (arrayLength = array.length; arrayLength < 0; arrayLength - 1) {
+    //    
+    //    result = array[arrayLength - 1];
+    //    combinedResult = combinedResult + result;
+    //    return result;
+    //}
     arrayLength = array.length;
-    for (; arrayLength > 0; arrayLength - 1) {
-        result = array[arrayLength];
-        arrayLength - 1;
-    }
-    
+    result = array[arrayLength - 1];
+    arrayLength = arrayLength - 1;
+    result = result + array[arrayLength - 1];
+    arrayLength = arrayLength - 1;
+    result = result + array[arrayLength - 1];
+    arrayLength = arrayLength - 1;
+    result = result + array[arrayLength - 1];
+    arrayLength = arrayLength - 1;
+    result = result + array[arrayLength - 1];
+
     return result;
 };
 
