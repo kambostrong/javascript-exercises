dda9937490a9f6599590323cac40c9b2203e571b
kambostrong <71000311+kambostrong@users.noreply.github.com>
2022-10-15 14:13:05 +1300

    working template saved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function(array, num) {
-    array.splice(num - 1, 1);
-    return array;
+//expect(removeFromArray([1, 2, 3, 4], 3, 2)).toEqual([1, 4]);
+
+const removeFromArray = function(array, ...num) {
+    //vvWORKS
+    // array.splice(num - 1, 1);
+    // return array;
+    //^^WORKS
+    
 };
 
 // Do not edit below this line
