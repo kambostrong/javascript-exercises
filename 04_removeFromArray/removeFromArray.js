2d130f2c23fe1a3384335709246d7114eab6a028
kambostrong <71000311+kambostrong@users.noreply.github.com>
2022-10-18 09:27:16 +1300

    2nd method working, no progress on 3 etc

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,24 @@
 //expect(removeFromArray([1, 2, 3, 4], 3, 2)).toEqual([1, 4]);
+//https://dmitripavlutin.com/foreach-iterate-array-javascript/
 
 const removeFromArray = function(array, ...num) {
     //vvWORKS
     // array.splice(num - 1, 1);
     // return array;
     //^^WORKS
+   
+
+    ////WORKS
+    // function index(item){
+    //     array.splice((array.indexOf(item)), 1);
+    //     return array;
+    // }
+    // num.forEach(index);
+    // return array;
+    ////WORKS
+
+
+
     
 };
 
