d66f7ff4789a61610bb668b66d7413879f261b1d
kambostrong <71000311+kambostrong@users.noreply.github.com>
2022-10-20 13:37:16 +1300

    ready to move forward

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,41 +2,16 @@
 //https://dmitripavlutin.com/foreach-iterate-array-javascript/
 
 const removeFromArray = function(array, ...num) {
-    //vvWORKS
-    // array.splice(num - 1, 1);
-    // return array;
-    //^^WORKS
-   
 
-    ////WORKS
-    // function index(item){
-    //     array.splice((array.indexOf(item)), 1);
-    //     return array;
-    // }
-    // num.forEach(index);
-    // return array;
-    ////WORKS
 
-for (let numLength = (num.length - 1); numLength >= 0; numLength = numLength - 1){
+
+    for (let numLength = (num.length - 1); numLength >= 0; numLength = numLength - 1){
     let indexToSplice = array.indexOf(num[numLength]);
     array.splice(indexToSplice, 1);
-    //return array;
 }
 
 
-// function callback(item, index, arr){
-//     array.splice(item, 1);
-//     return array;
-// }
-// num.forEach(callback);
-// return array;
 
-
-//WORKS!! Why doesn't function work?
-// let numLength = num.length - 1;
-// let indexToSplice = array.indexOf(num[numLength]);
-//     array.splice(indexToSplice, 1);
-//WORKS!! Why doesn't function work?
 return array;
 
 };
