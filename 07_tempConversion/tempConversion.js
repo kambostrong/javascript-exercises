a1862e23955ddb618a05023964a287b47b24c780
kambostrong <71000311+kambostrong@users.noreply.github.com>
2022-10-21 22:36:18 +1300

    initial commit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let fAnswer = ((f - 32) * 1.8);
+  return fAnswer.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let cAnswer = ((c * 1.8) + 32);
+  return cAnswer.toFixed(1);
 };
+
+
+//x°C ≘ (x × 1.8) + 32 °F	
+//x°F ≘ (x − 32) × 1.8 °C
 
 // Do not edit below this line
 module.exports = {
