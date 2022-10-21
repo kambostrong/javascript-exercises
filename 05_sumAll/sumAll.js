751067476b3e0aa8bf15671c4d60862a71be1fb3
kambostrong <71000311+kambostrong@users.noreply.github.com>
2022-10-21 13:42:30 +1300

    initial draft, working

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
 
+
+
+const sumAll = function(a, z) {
+let smaller = 0;
+let larger = 0;
+let finalSum = 0;
+if (a > z){
+    larger = a;
+    smaller = z;
+} else if (z > a){
+    larger = z;
+    smaller = a;
 };
 
-// Do not edit below this line
+
+    for (let counter = smaller; counter < (larger + 1); counter++) {
+        finalSum += counter;
+    }
+
+
+return finalSum;
+
+};
+//sumAll(1,4);
+
+
+//Do not edit below this line
 module.exports = sumAll;
