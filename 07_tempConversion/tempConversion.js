d6197c44bde939d92084e6fe1b6ecd5ce046d59d
kambostrong <71000311+kambostrong@users.noreply.github.com>
2022-10-22 13:25:16 +1300

    works! used nice methods here.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
 const ftoc = function(f) {
-  let fAnswer = ((f - 32) * 1.8);
-  return fAnswer.toFixed(1);
+  let fAnswer = ((f - 32) * 5/9);
+  fAnswer = fAnswer.toFixed(1);
+  return +fAnswer;
 };
 
 const ctof = function(c) {
-  let cAnswer = ((c * 1.8) + 32);
-  return cAnswer.toFixed(1);
+  let cAnswer = ((c * (9/5)) + 32);
+  cAnswer = cAnswer.toFixed(1);
+  return +cAnswer;
 };
 
 
