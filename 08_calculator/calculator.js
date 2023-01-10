7ce282c0085f22729573b9dca1660fc985a7c1ce
orzcode <71000311+orzcode@users.noreply.github.com>
2023-01-10 16:32:40 +1300

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(value1, value2) {
+  return value1 + value2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(value1, value2) {
+	return value1 - value2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    return array.reduce((cumul, current) => (cumul + current), 0);   
 };
 
-const multiply = function() {
 
+const multiply = function(array) {
+  return array.reduce((cumul, current) => (cumul * current));
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return (number ** power);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number === 1){
+    return number;
+  } else if(number === 0){
+    return 1;
+  } 
+  return number * factorial(number-1);
 };
 
 // Do not edit below this line
