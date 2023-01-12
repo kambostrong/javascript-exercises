a7dd6c3bb5e68568f94b52252577c874212f8c23
orzcode <71000311+orzcode@users.noreply.github.com>
2023-01-12 13:18:21 +1300

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+	let noSym = sentence.replace(/[!,. ]/g, '');
+	//removes punctuation and spaces
+	noSym = noSym.toLowerCase();
+	//converts to lowercase
+	noSym = [...noSym];
+	//converts string to array
+	let noSymReverse = [...noSym].reverse();	
+	// //creates duplicate, in reverse
+	let stringA = noSym.toString();
+	let stringB = noSymReverse.toString();
+	//converts to string (adds in commas unfortunately, but not really an issue)
+	if (stringA === stringB){
+		return true
+	}	else {return false}
+	
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+	if (number <= 0){
+		return "OOPS"
+	}	else {
+	let array = [0, 1];
+	for(let i = 1; i < number; i++){		  
+    array.push(array[i] + array[i-1])		
+	}
+	return array.pop();
+}
 };
 
 // Do not edit below this line
