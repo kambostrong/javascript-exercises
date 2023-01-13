3e12612c7e4eff449d5fb6b6d492f6dde6d8fecc
orzcode <71000311+orzcode@users.noreply.github.com>
2023-01-13 16:59:38 +1300

    fuck this

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	function getAge(death, birth){
+		if (death === undefined){
+			return ((new Date().getFullYear()) - birth);
+		}
+		else return (death - birth);
+	}
+	
+	function callbackFN(accum, curr){
+		const oldestAge = getAge(accum.yearOfDeath, accum.yearOfBirth);
+		const currAge = getAge(curr.yearOfDeath, curr.yearOfBirth);
+		if (oldestAge > currAge){
+			return accum;
+		} else return curr;
+	}
+
+	return people.reduce(callbackFN);
 
 };
 
